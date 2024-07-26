a5f67f624e08ec10ec823858d291ef5c47beef37
Dharanidharan K <75402352+DHARANIDHARANK@users.noreply.github.com>
2024-07-26 10:33:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 cd loginpage
 docker build -t this23/quizapp:loginpage .
+docker push this23/quizapp:loginpage
 
 cd ..
 
 cd backend 
 docker build -t this23/quizapp:quizback .
-
+docker push this23/quizapp:quizback
 
 cd ..
